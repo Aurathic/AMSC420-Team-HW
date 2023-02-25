3a2ab657063f36cbc4ad0bf54d1ca938b8673e9c
Adam <39442291+Aurathic@users.noreply.github.com>
2023-02-24 20:18:57 -0500

    Change J iteration to actually use gamma

--- minimizeGamma.m
+++ minimizeGamma.m
@@ -1,9 +1,10 @@
-function [gamma] = minimizeGamma(t0, Tmax, Y, Rsim, p)
+function [gamma, minVal] = minimizeGamma(t0, Tmax, Y, Rsim, p)
     %MINIMIZEGAMMA Find gamma which minimizes residuals
     % Find argmin for gamma over the p-norm of (Y - gamma*Rsim)
     % p should be 1, 2, or infinity (inf)
+    % Also returns the minimized value.
     
     Ycut = Y(t0:t0+Tmax);
     func = @(g) norm(Ycut-g*Rsim, p);
-    gamma = fminsearch(func, 1);
+    [gamma, minVal] = fminsearch(func, 1);
 end
